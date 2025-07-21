9706bfbbf0258ffe79da17c2ea00bf845624fa28
Anthony Munoz <acmunozq@gmail.com>
2025-07-20 23:41:51 -0500

    update securrity

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8n9xzWmE8D6nkcKGZR20wK2svuImleww",
+    authDomain: "wasicash-comentarios.firebaseapp.com",
+    databaseURL: "https://wasicash-comentarios-default-rtdb.firebaseio.com",
+    projectId: "wasicash-comentarios",
+    storageBucket: "wasicash-comentarios.firebasestorage.app",
+    messagingSenderId: "604186292458",
+    appId: "1:604186292458:web:4930059c07ccbb19fe2793",
+    measurementId: "G-7B181THR7T"
+};
+firebase.initializeApp(firebaseConfig);
